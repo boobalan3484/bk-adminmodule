48bd34396c7f3f0bfbe2fde0f913ccefb0aa75d6
visvesvaran-S <visvesvaran08@gmail.com>
2024-03-09 12:45:06 +0530

    srikanth

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,7 @@ function App() {
 
   return (
     <div>
-<<<<<<< HEAD
-      <h1 className="text-3xl font-bold underline">
-      vankkam!
-    </h1>
-=======
       <Auth/>
->>>>>>> 47c9b8c64af94a7a24d05eab238e4af80c9fd5e7
     </div>
   )
 }
