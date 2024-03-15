a9cf5e768ebdc1372d6c32d1801dbf415e74c2a2
boobalan3484 <boobalan3484@gmail.com>
2024-03-15 13:02:54 +0530

    NA

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -6,17 +6,12 @@ import { Route, Routes } from 'react-router-dom'
 function Auth() {
     return (
         <div>
-          
-       <Routes>
-<<<<<<< HEAD:src/Auth.jsx
-
-=======
->>>>>>> 5532d189096e23a78157f11a0397f087f451f1a6:src/Components/Auth.jsx
-        <Route path="/" element={<Login />} />
-        <Route path="Register" element={<Register />} />
-       </Routes>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="Register" element={<Register />} />
+            </Routes>
         </div>
-    )   
+    )
 }
 
 export default Auth
