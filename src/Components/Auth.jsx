79551a2ae2f1a267e3788edc1a0f33ff830ce780
boobalan3484 <boobalan3484@gmail.com>
2024-03-15 13:05:53 +0530

    NA

---
+++ src/Components/Auth.jsx
@@ -0,0 +1,17 @@
+import Login from './Login'
+import Register from './Register'
+
+import { Route, Routes } from 'react-router-dom'
+
+function Auth() {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="Register" element={<Register />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Auth
